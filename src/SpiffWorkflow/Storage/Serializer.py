e5c5c19a58685b79494abd6252f9af346d840113
Filipe Correia <fcorreia@gmail.com>
2012-02-10 16:08:11 +0000

    Moved DictionarySerializer to the tests. It's an example of how serialization can be implemented, rather than a deployable serialization strategy.

--- src/SpiffWorkflow/Storage/Serializer.py
+++ src/SpiffWorkflow/Storage/Serializer.py
@@ -15,13 +15,6 @@
 import sys
 from SpiffWorkflow.Task import Task
 
-def get_class(full_class_name):
-    parts = full_class_name.rsplit('.', 1)
-    module_name = parts[0]
-    class_name = parts[1]
-    __import__(module_name)
-    return getattr(sys.modules[module_name], class_name)
-
 class Serializer(object):
     def serialize_job(self, job):
         raise NotImplementedError("You must implement the serialize_job method.")
@@ -29,44 +22,6 @@ class Serializer(object):
     def deserialize_job(self, s_state):
         raise NotImplementedError("You must implement the deserialize_job method.")
 
-class DictionarySerializer(Serializer):
-    def serialize_job(self, job):
-        s_state = dict()
-        s_state['attributes'] = job.attributes
-        s_state['last_task'] = job.last_task.id if not job.last_task is None else None
-        s_state['success'] = job.success
-        s_state['task_tree'] = [self.serialize_task(task) for task in Task.Iterator(job.task_tree)]
-        s_state['workflow'] = job.workflow.__class__.__module__ + '.' + job.workflow.__class__.__name__
-        return s_state
 
-    def deserialize_job(self, s_state):
-        from SpiffWorkflow.Job import Job
-        wf_class = get_class(s_state['workflow'])
-        wf = wf_class()
-        job = Job(wf)
-        job.attributes = s_state['attributes']
-        job.last_task = s_state['last_task']
-        job.success = s_state['success']
-        tasks = [self.deserialize_task(job, serialized_task) for serialized_task in s_state['task_tree']]
-        job.task_tree = [task for task in tasks if task.spec.name == 'Root'][0]
-        job.workflow = wf
-        return job
-
-    def serialize_task(self, task):
-        s_state = dict()
-        s_state['id'] = task.id
-        s_state['state'] = task.state
-        s_state['last_state_change'] = task.last_state_change
-        s_state['attributes'] = task.attributes
-        s_state['internal_attributes'] = task.internal_attributes
-        return s_state
-
-    def deserialize_task(self, job, s_state):
-        task = job.get_task(s_state['id'])
-        task.state = s_state['state']
-        task.last_state_change = s_state['last_state_change']
-        task.attributes = s_state['attributes']
-        task.internal_attributes = s_state['internal_attributes']
-        return task
 
 
--- tests/SpiffWorkflow/PersistSmallWorkflowTest.py
+++ tests/SpiffWorkflow/PersistSmallWorkflowTest.py
@@ -6,11 +6,11 @@ from SpiffWorkflow.Tasks     import *
 from SpiffWorkflow.Operators import *
 from SpiffWorkflow.Task      import *
 from SpiffWorkflow.Tasks.Simple import Simple
-from SpiffWorkflow.Storage import DictionarySerializer
+from SpiffWorkflow.Storage   import Serializer
 
 class ASmallWorkflow(Workflow):
     def __init__(self):
-        super(ASmallWorkflow, self).__init__(name = "asmallworkflow")
+        super(ASmallWorkflow, self).__init__(name = "A Small Workflow")
 
         multichoice = MultiChoice(self, 'multi_choice_1')
         self.start.connect(multichoice)
@@ -28,6 +28,53 @@ class ASmallWorkflow(Workflow):
 
         end = Simple(self, 'End')
         syncmerge.connect(end)
+
+def get_class(full_class_name):
+    parts = full_class_name.rsplit('.', 1)
+    module_name = parts[0]
+    class_name = parts[1]
+    __import__(module_name)
+    return getattr(sys.modules[module_name], class_name)
+
+class DictionarySerializer(Serializer):
+    def serialize_job(self, job):
+        s_state = dict()
+        s_state['attributes'] = job.attributes
+        s_state['last_task'] = job.last_task.id if not job.last_task is None else None
+        s_state['success'] = job.success
+        s_state['task_tree'] = [self.serialize_task(task) for task in Task.Iterator(job.task_tree)]
+        s_state['workflow'] = job.workflow.__class__.__module__ + '.' + job.workflow.__class__.__name__
+        return s_state
+
+    def deserialize_job(self, s_state):
+        from SpiffWorkflow.Job import Job
+        wf_class = get_class(s_state['workflow'])
+        wf = wf_class()
+        job = Job(wf)
+        job.attributes = s_state['attributes']
+        job.last_task = s_state['last_task']
+        job.success = s_state['success']
+        tasks = [self.deserialize_task(job, serialized_task) for serialized_task in s_state['task_tree']]
+        job.task_tree = [task for task in tasks if task.spec.name == 'Root'][0]
+        job.workflow = wf
+        return job
+
+    def serialize_task(self, task):
+        s_state = dict()
+        s_state['id'] = task.id
+        s_state['state'] = task.state
+        s_state['last_state_change'] = task.last_state_change
+        s_state['attributes'] = task.attributes
+        s_state['internal_attributes'] = task.internal_attributes
+        return s_state
+
+    def deserialize_task(self, job, s_state):
+        task = job.get_task(s_state['id'])
+        task.state = s_state['state']
+        task.last_state_change = s_state['last_state_change']
+        task.attributes = s_state['attributes']
+        task.internal_attributes = s_state['internal_attributes']
+        return task
 
 class PersistSmallWorkflowTest(unittest.TestCase):
     """Runs persistency tests agains a small and easy to inspect workflowdefinition"""
